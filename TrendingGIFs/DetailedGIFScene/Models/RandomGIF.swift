2d283f444d308d6a8bc118f103864a27ddef098b
Sana <sanaelshazly1993@gmail.com>
2019-05-10 15:55:25 +0200

    Add random gif model

---
+++ TrendingGIFs/DetailedGIFScene/Models/RandomGIF.swift
@@ -0,0 +1,13 @@
+//
+//  RandomGIF.swift
+//  TrendingGIFs
+//
+//  Created by Sana Elshazly on 5/9/19.
+//  Copyright © 2019 Sana Elshazly. All rights reserved.
+//
+
+import Foundation
+
+struct RandomGIF: Codable {
+    let data: GIF
+}
